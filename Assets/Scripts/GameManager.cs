3e51776e38e2baf37c2bae68aa9e8c8d6a627891
sanidhya-commits <sanidhyaniranjan2006@gmail.com>
2026-01-15 00:39:26 +0530

    Basic Gate Functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,10 @@ public class GameManager : MonoBehaviour
 {
     public GameObject baseCar;
     public GameObject spawnPoint;
+    public GameObject doorHinge1, doorHinge2;
 
     private GameObject spawnedCar;
+    private bool canPass = false;
 
 
 
@@ -15,15 +17,32 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E)) {
             Spawn();
         }
-        if (spawnedCar != null) {
+        if (spawnedCar != null && spawnedCar.transform.position.x < 0) {
             Vector2 carPos = spawnedCar.transform.position;
             carPos += new Vector2 (10*Time.deltaTime, 0);
+            spawnedCar.transform.position = carPos;
+        }
+
+        if(spawnedCar != null && spawnedCar.transform.position.x >= 0 && canPass == true) {
+            Vector2 carPos = spawnedCar.transform.position;
+            carPos += new Vector2(10 * Time.deltaTime, 0);
             spawnedCar.transform.position = carPos;
         }
 
         if(spawnedCar != null && spawnedCar.transform.position.x >= 13) {
             Destroy(spawnedCar);
             spawnedCar = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.N)) {
+            doorHinge1.transform.rotation = Quaternion.Euler(0, 0, -90);
+            doorHinge2.transform.rotation = Quaternion.Euler(0, 0, -90);
+            canPass = true;
+        }
+        if (Input.GetKeyDown(KeyCode.M)) {
+            doorHinge1.transform.rotation = Quaternion.Euler(0, 0, 0);
+            doorHinge2.transform.rotation = Quaternion.Euler(0, 0, 180);
+            canPass = false;
         }
     }
 
