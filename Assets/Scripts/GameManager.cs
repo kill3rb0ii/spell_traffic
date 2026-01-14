6bb5c05d536182eafe1c0e6ce8c7ded320196c5f
sanidhya-commits <sanidhyaniranjan2006@gmail.com>
2026-01-14 23:38:27 +0530

    Basic Car spawning and despawning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject baseCar;
+    public GameObject spawnPoint;
+
+    private GameObject spawnedCar;
+
+
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            Spawn();
+        }
+        if (spawnedCar != null) {
+            Vector2 carPos = spawnedCar.transform.position;
+            carPos += new Vector2 (10*Time.deltaTime, 0);
+            spawnedCar.transform.position = carPos;
+        }
+
+        if(spawnedCar != null && spawnedCar.transform.position.x >= 13) {
+            Destroy(spawnedCar);
+            spawnedCar = null;
+        }
+    }
+
+    void Spawn() {
         
+        if (spawnedCar == null) {
+            spawnedCar = Instantiate(baseCar, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        }
+        else {
+            Debug.Log("Car is already spawned");
+        }
     }
 }
